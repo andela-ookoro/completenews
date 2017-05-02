cb46de23be657b0d327aa9970f3acf26ddb3e7cb
celestine <okorocelestine@gmail.com>
2017-05-02 01:01:56 +0100

    add links and menu bar

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -17,16 +17,16 @@ class Layout extends React.Component {
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>                        
 				  </button>
-				  <a class="navbar-brand" href="myPage" style={{color:'#FF8040'}}>Issue Tracker</a>
+				  <a class="navbar-brand" href="myPage" style={{color:'#FF8040'}}>Complete News</a>
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 				  <ul class="nav navbar-nav navbar-right">
-					<li><Link to="archives" activeClassName="activelink"> <span>Archives </span></Link></li>
+					<li><Link to="headlines" activeClassName="activelink"> <span>Headlines </span></Link></li>
 					<li><Link to="setting" activeClassName="activelink"><span> Setting</span> </Link></li>
-					<li><Link to="featured" activeClassName="activelink"> <span>Featured</span> </Link></li>
+					<li><Link to="favorite" activeClassName="activelink"> <span>Favorite</span> </Link></li>
 					<li><Link to="todo" activeClassName="activelink"><span> Todo list</span> </Link></li>
 					<li><a href="\signout">Sign out</a></li>
-					 <li ><a id="usernamet" ></a></li>
+					<li style={{color:'#FF8040'}} ><span id="usernamet" ref="username">Okoro Okwudiri</span></li>
 				  </ul>
 				</div>
 			  </div>
