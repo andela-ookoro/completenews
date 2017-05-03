8b9a05a9ca082ff90a1f1d4be3e10be2742be618
celestine <okorocelestine@gmail.com>
2017-05-03 05:36:51 +0100

    remove unnecessary route

--- src/js/client.js
+++ src/js/client.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route,Router,hashHistory,IndexRoute} from 'react-router';
 import Layout from './pages/layout';
-import Featured from './pages/Featured';
-import Setting from './pages/Setting';
-import Archives from './pages/Archives';
-import Todo from './pages/Todo';
 import Headlines from './pages/Headlines';
 import Favorite from './pages/Favorite';
 
@@ -16,9 +12,6 @@ ReactDOM.render(
 			<IndexRoute  component={Headlines} ></IndexRoute>
 			<Route path="archives/:article" name="archive" component={Archives} ></Route>
 			<Route path="headlines" component={Headlines} ></Route>
-			<Route path="favorite" component={Favorite} ></Route>
-			<Route path="featured" component={Featured} ></Route>
-			<Route path="todo" component={Todo} ></Route>
 		</Route>
 	</Router>
 ,app);
