6b14f3825ccd40092d3eb330d812f0419f18ef67
Okoro Celestine <okorocelestine@gmail.com>
2017-05-05 19:20:40 +0100

    change list of sources to link

--- src/js/pages/headlines/SourceOptions.jsx
+++ src/js/pages/headlines/SourceOptions.jsx
@@ -7,6 +7,6 @@ class SourceOptions extends React.Component {
       <option value={this.props.data.id } title={this.props.data.description}>{this.props.data.name}</option>
     );
   }
-}
+} 
 
 export default SourceOptions;
