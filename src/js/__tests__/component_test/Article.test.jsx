1b124ea07d84ed9cf30db592197784786baa1dfd
Okoro Celestine <okorocelestine@gmail.com>
2017-05-10 14:56:48 +0100

    add test for components

---
+++ src/js/__tests__/component_test/Article.test.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Renderer from 'react-test-renderer';
+import Article from 'Article';
+
+test('Component render the article template', () => {
+  const article = {
+    author: 'celestine',
+    title: 'My story',
+    urlToImage: 'test.jpg',
+    publishedAt: '24/12/24',
+    isAuth: true,
+    url: 'http://localhost',
+  };
+  const funtest = () => {
+    return 1;
+  };
+  const i = 1;
+  const source = 'BBC';
+  const isAuth= true;
+  const component = Renderer.create(
+    <Article
+      key={i} id={i} author={article.author} title={article.title}
+      urlToImage={article.urlToImage} description={article.description}
+      publishedAt={article.publishedAt} url={article.url} source={source}
+      isAuth={isAuth}
+    />,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Value of text of each control should be same with props property', () => {
+  const article = {
+    author: 'celestine',
+    title: 'My story',
+    urlToImage: 'test.jpg',
+    publishedAt: '24/12/24',
+    url: 'http://localhost',
+  };
+  const onClick = jest.fn();
+  const i = 1;
+  const source = 'BBC';
+  const isAuth = true;
+  const wrapper = mount(
+    <Article
+      key={i} id={i} author={article.author} title={article.title}
+      urlToImage={article.urlToImage} description={article.description}
+      publishedAt={article.publishedAt} url={article.url} source={source}
+      isAuth={isAuth}
+    />,
+  );
+  const heading = wrapper.find('h6');
+  expect(heading.text()).toEqual(`${article.author}:   ${article.title}`);
+  const img = wrapper.find('img').props('urlToImage');
+  expect(img.src).toEqual(article.urlToImage);
+  const sourceURl = wrapper.find('a').props('url');
+  expect(sourceURl.href).toEqual(article.url);
+  expect(sourceURl.children).toEqual(`Read on  ${source} `);
+  expect(sourceURl.target).toEqual('_blank');
+  const button = wrapper.find('button').props('className');
+  const className = button.className;
+  expect(!className.includes('disabled')).toEqual(true);
+  // expect(wrapper.getDOMNode()).to.have.property('card-image');
+});
+
+test('Add to favourite button should be disable for anonymous users', () => {
+  const article = {
+    author: 'celestine',
+    title: 'My story',
+    urlToImage: 'test.jpg',
+    publishedAt: '24/12/24',
+    url: 'http://localhost',
+  };
+  const i = 1;
+  const source = 'BBC';
+  const isAuth = false;
+  const wrapper = mount(
+    <Article
+      key={i} id={i} author={article.author} title={article.title}
+      urlToImage={article.urlToImage} description={article.description}
+      publishedAt={article.publishedAt} url={article.url} source={source}
+      isAuth={isAuth}
+    />,
+  );
+  const button = wrapper.find('button').props('className');
+  const className = button.className;
+  expect(className.includes('disabled')).toEqual(true);
+});
+
+
---
+++ src/js/__tests__/component_test/SourceOptions.test.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Renderer from 'react-test-renderer';
+import SourceOptions from 'SourceOptions';
+
+test('Component render a button', () => {
+  const source = {
+    id: 'BBC',
+    name: 'BBC News',
+    description: 'BBC world news',
+  };
+  const funtest = () => {
+    return 1;
+  };
+  const component = Renderer.create(
+    <SourceOptions
+      key={source.id} name={source.name} title={source.description}
+      id={source.id} fetchAvailableSort={funtest}
+    />,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Value of render button should be same as props', () => {
+  const source = {
+    id: 'BBC',
+    name: 'BBC News',
+    description: 'BBC world news',
+  };
+  const funtest = () => {
+    return 1;
+  };
+  const component = mount(
+    <SourceOptions
+      key={source.id} name={source.name} title={source.description}
+      id={source.id} fetchAvailableSort={funtest}
+    />,
+  );
+  const link = component.find('a');
+  expect(link.text()).toEqual(source.name);
+});
+
+test('Link onclick should execute the function in the prop', () => {
+  const source = {
+    id: 'BBC',
+    name: 'BBC News',
+    description: 'BBC world news',
+  };
+  const onClick = jest.fn();
+  const component = mount(
+    <SourceOptions
+      key={source.id} name={source.name} title={source.description}
+      id={source.id} fetchAvailableSort={onClick}
+    />,
+  );
+  const link = component.find('a');
+  link.simulate('click');
+  expect(onClick).toBeCalled();
+});
