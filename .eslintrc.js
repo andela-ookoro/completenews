35d68ddb191aea2153a841bc1d06735274a654fb
andela_ookoro <okwudiri.okoro@andela.com>
2017-05-23 05:36:08 +0100

    disable no-nested-ternary and jsx-a11y/href-no-hash rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,26 +17,28 @@ module.exports = {
 		"quote-props": "off",
 		"jsx-a11y/no-static-element-interactions": "off",
 		"one-var": 0,
-    "one-var-declaration-per-line": 0,
-    "new-cap": 0,
-    "consistent-return": 0,
-    "no-param-reassign": 0,
-    "comma-dangle": 0,
-    "curly": ["error", "multi-line"],
-    "no-shadow": ["error", { "allow": ["req", "res", "err"] }],
-    "valid-jsdoc": ["error", {
-      "requireReturn": true,
-      "requireReturnType": true,
-      "requireParamDescription": false,
-      "requireReturnDescription": true
-    }],
-    "require-jsdoc": ["error", {
-        "require": {
-            "FunctionDeclaration": true,
-            "MethodDefinition": true,
-            "ClassDeclaration": true
-        }
-    }]
+        "jsx-a11y/href-no-hash": 0,
+        "one-var-declaration-per-line": 0,
+        "new-cap": 0,
+        "consistent-return": 0,
+        "no-param-reassign": 0,
+        "comma-dangle": 0,
+        "no-nested-ternary": 0,
+        "curly": ["error", "multi-line"],
+        "no-shadow": ["error", { "allow": ["req", "res", "err"] }],
+        "valid-jsdoc": ["error", {
+        "requireReturn": true,
+        "requireReturnType": true,
+        "requireParamDescription": false,
+        "requireReturnDescription": true
+        }],
+        "require-jsdoc": ["error", {
+            "require": {
+                "FunctionDeclaration": true,
+                "MethodDefinition": true,
+                "ClassDeclaration": true
+            }
+        }]
 	},
 	"env": {
     "jest": true
